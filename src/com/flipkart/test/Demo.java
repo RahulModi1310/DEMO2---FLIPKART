a6ab189181afb599b40112e6c487cea4e9342c65
Rahul Ketan  Modi <rahul_km@ee.iitr.ac.in>
2024-06-25 15:33:59 +0530

    DEMO2 Project Initialized

---
+++ src/com/flipkart/test/Demo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.flipkart.test;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Java Program");
+	}
+}
